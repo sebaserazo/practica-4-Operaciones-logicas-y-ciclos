279a9c754a2ebc90bb2a5e4f3afa252f65182871
sebaserazo <107583260+sebaserazo@users.noreply.github.com>
2023-06-30 00:20:19 -0300

    Create java12.java

---
+++ java12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AND {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa un número: ");
+        int numero = scanner.nextInt();
+
+        String docena = determinarDocenaAnd(numero);
+
+        System.out.println(docena);
+
+        scanner.close();
+    }
+
+    public static String determinarDocenaAnd(int numero) {
+        if (numero >= 1 && numero <= 12) {
+            return "El número " + numero + " pertenece a la primer docena.";
+        } else if (numero >= 13 && numero <= 24) {
+            return "El número " + numero + " pertenece a la segunda docena.";
+        } else if (numero >= 25 && numero <= 36) {
+            return "El número " + numero + " pertenece a la tercer docena.";
+        } else {
+            return "El número " + numero + " está fuera de rango.";
+        }
+    }
+}
