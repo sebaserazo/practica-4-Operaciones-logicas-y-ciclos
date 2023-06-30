97383df60bb46a6128993a9ca81621cf92224749
sebaserazo <107583260+sebaserazo@users.noreply.github.com>
2023-06-29 23:59:36 -0300

    Create java3.java

---
+++ java3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CantidadDiasMes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el nombre del mes: ");
+        String nombreMes = scanner.nextLine();
+
+        int cantidadDias;
+
+        switch (nombreMes.toLowerCase()) {
+            case "enero":
+            case "marzo":
+            case "mayo":
+            case "julio":
+            case "agosto":
+            case "octubre":
+            case "diciembre":
+                cantidadDias = 31;
+                break;
+            case "febrero":
+                cantidadDias = 28;
+                break;
+            case "abril":
+            case "junio":
+            case "septiembre":
+            case "noviembre":
+                cantidadDias = 30;
+                break;
+            default:
+                cantidadDias = -1; // Valor inválido para el nombre del mes
+                break;
+        }
+
+        if (cantidadDias != -1) {
+            System.out.println("El mes de " + nombreMes + " tiene " + cantidadDias + " días.");
+        } else {
+            System.out.println("Nombre de mes inválido.");
+        }
+
+        scanner.close();
+    }
+}
