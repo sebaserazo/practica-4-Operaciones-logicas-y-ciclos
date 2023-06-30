c48d2c1925124716b567a595b2795e836fb6e81c
sebaserazo <107583260+sebaserazo@users.noreply.github.com>
2023-06-30 00:33:42 -0300

    Update java11.java

--- java11.java
+++ java11.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
-public class {
-    public static void main (String[] args){
+public class DeterminarVocal {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese el caracter");
-        char caracter = scanner.nextLine().charAt(0);
-        
-        if( caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u')
-            System.out.println("El caracter es vocal");
-        else
-            System.out.println("El caracter es conosonante");
+        System.out.print("Ingrese un carácter: ");
+        char caracter = scanner.next().charAt(0);
+
+        boolean esVocal = (caracter == 'a') || (caracter == 'e') || (caracter == 'i') || (caracter == 'o') || (caracter == 'u');
+
+        if (esVocal) {
+            System.out.println("El carácter ingresado es una vocal.");
+        } else {
+            System.out.println("El carácter ingresado no es una vocal.");
+        }
     }
 }
