5f4c6e88b6cbcea16192cec69acbe730ef158d06
sebaserazo <107583260+sebaserazo@users.noreply.github.com>
2023-06-30 00:28:49 -0300

    Create java17a.java

---
+++ java17a.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumaValoresPares {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad de números a sumar: ");
+        int cantidadNumeros = scanner.nextInt();
+
+        int sumaPares = 0;
+
+        for (int i = 1; i <= cantidadNumeros; i++) {
+            System.out.print("Ingrese el número " + i + ": ");
+            int numero = scanner.nextInt();
+
+            if (numero % 2 == 0) {
+                sumaPares += numero;
+            }
+        }
+
+        System.out.println("La suma de los números pares es: " + sumaPares);
+    }
+}
